c3ca269c7ccc6288adcd0e26aaefb81bd00444bf
femell0509 <kirillvah.com@gmail.com>
2024-11-05 13:06:59 +0300

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,11 @@
+
+def apply_all_func(int_list:list, *functions):
+    results = {}
+    for function in functions:
+        results.update({function.__name__: function(int_list)})
+    return results
+
+
+if __name__ == '__main__':
+    print(apply_all_func([6, 20, 15, 9], max, min))
+    print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
